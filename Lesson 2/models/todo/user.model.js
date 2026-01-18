af81ab35b6a3af41e544e81d555614e6099d5b28
Anuj Katare <anujkatare2006@gmail.com>
2026-01-18 13:09:37 +0530

    Mongoose Lesson 2

--- Lesson 2/models/todo/user.model.js
+++ Lesson 2/models/todo/user.model.js
@@ -1,13 +1,13 @@
 import mongoose from  'mongoose';
-import { type } from 'os';
+
 
 const userSchema = new mongoose.Schema({
     username: {
         type:String,
         required:true,
-        unique: true,
         lowercase: true
-    },
+    },   unique: true,
+     
     email: {
         type: String,
         required: true,
